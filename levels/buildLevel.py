039ff1abb54377bb76bf26b2ae7dd10eb46159e8
ZiarayZ <jakesmith@binaryrage.com>
2022-05-30 12:51:58 +0100

    restrict level overwrite
    only for the default 1-11 levels

--- levels/buildLevel.py
+++ levels/buildLevel.py
@@ -1,6 +1,15 @@
 from cryptography.fernet import Fernet
 import os
 
+test = levelName = ""
+def looped():
+    print("Default levels 1-11, name your levels 12+")
+    levelName = input("\nSpecify name of map.\nFor example: \'level_12\'\n\n>: ").lower().strip() + ".bin"
+    levelName = levelName.replace(".txt", "").replace(".bin.bin", ".bin")
+    if "level_" in levelName:
+        test = levelName.replace("level_","").replace(".bin","")
+        test = int(test)
+
 file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "key.key"), "rb")
 load_key = file.read()
 levelKey = Fernet(load_key)
@@ -23,16 +32,17 @@ while running:
     file.close()
     level = level.rstrip()
     print("\nTranslated level.")
+    looped()
 
-    print("Default levels 1-11, name your levels 12+")
-    levelName = input("\nSpecify name of map.\nFor example: \'level_12\'\n\n>: ").lower().strip() + ".bin"
-    levelName = levelName.replace(".txt", "").replace(".bin.bin", ".bin")
-
-    file = open(os.path.join("maps", levelName), "wb")
-    #translate decrypted to encrypted
-    file.write(levelKey.encrypt(level.encode()))
-    file.close()
-    print("Level creation finished successfully.")
+    while test >= 1 and test <= 11:
+        print("You cannot replace the original 11 levels!!")
+        looped()
+    else:
+        file = open(os.path.join("maps", levelName), "wb")
+        #translate decrypted to encrypted
+        file.write(levelKey.encrypt(level.encode()))
+        file.close()
+        print("Level creation finished successfully.")
 
     again = input("Would you like to load another level?\n\n>: ").lower().strip()
     if again == "no" or again == "n":
